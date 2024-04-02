7a149e443aec022884f125910f79ea703b71a7c0
Victor Alejandro Arévalo Díaz <u202112543@upc.edu.pe>
2024-04-02 11:07:50 -0500

    summ

---
+++ src/main/java/pe/edu/upc/verisoft_backend_1/VerisoftBackend1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.verisoft_backend_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerisoftBackend1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerisoftBackend1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/verisoft_backend_1/VerisoftBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.verisoft_backend_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerisoftBackend1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
