a7c4879f18aa4aaa7c4b5748d7b870c24af32a03
Nicoll23 <108904889+Nicoll23@users.noreply.github.com>
2024-04-27 03:27:07 -0500

    Querys Cirugias por paciente y Alergias por paciente

--- src/main/java/pe/edu/upc/verisoft_backend_1/controllers/AlergiasController.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/controllers/AlergiasController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("alergia")
+@RequestMapping("alergias")
 public class AlergiasController {
     @Autowired
     private IAlergiasService aS;
