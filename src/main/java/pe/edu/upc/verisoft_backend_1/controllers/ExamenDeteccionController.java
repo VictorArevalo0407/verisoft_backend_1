45a17cf0923dc3e80e27613c5484195e43d074d5
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-04-27 10:29:43 -0500

    Modificar ExamenDeteccion
    Modificar

--- src/main/java/pe/edu/upc/verisoft_backend_1/controllers/ExamenDeteccionController.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/controllers/ExamenDeteccionController.java
@@ -4,7 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.verisoft_backend_1.dtos.ExamenDeteccionDTO;
+import pe.edu.upc.verisoft_backend_1.dtos.PacienteDTO;
 import pe.edu.upc.verisoft_backend_1.entities.ExamenDeteccion;
+import pe.edu.upc.verisoft_backend_1.entities.Paciente;
 import pe.edu.upc.verisoft_backend_1.servicesinterfaces.IExamenDeteccionService;
 
 import java.util.List;
@@ -19,6 +21,12 @@ public class ExamenDeteccionController {
     public void isertar(@RequestBody ExamenDeteccionDTO examenDeteccionDTO){
         ModelMapper d=new ModelMapper();
         ExamenDeteccion examenDeteccion=d.map(examenDeteccionDTO, ExamenDeteccion.class);
+        eS.insert(examenDeteccion);
+    }
+    @PutMapping
+    public void modificar(@RequestBody ExamenDeteccionDTO examenDeteccionDTO) {
+        ModelMapper m = new ModelMapper();
+        ExamenDeteccion examenDeteccion = m.map(examenDeteccionDTO, ExamenDeteccion.class);
         eS.insert(examenDeteccion);
     }
     @GetMapping
