207cc057daef310c9ab115836c595360747ac9f2
Usuario X <majuv938@gmail.com>
2024-06-11 13:44:14 -0500

    Commit Prueba
    Commit Prueba

--- src/main/java/pe/edu/upc/verisoft_backend_1/controllers/TipoTratamientoController.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/controllers/TipoTratamientoController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/tipotratamientos")
 public class TipoTratamientoController {
+    //a
     @Autowired
     private ITipoTratamientoService ttS;
     @PostMapping
