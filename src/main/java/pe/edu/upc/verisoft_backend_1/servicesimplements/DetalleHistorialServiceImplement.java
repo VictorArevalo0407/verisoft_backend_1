d65ea97c52c8c65d9cc5eabb26c60fbca3993e0d
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-06-22 16:19:56 -0500

    cambio querys

--- src/main/java/pe/edu/upc/verisoft_backend_1/servicesimplements/DetalleHistorialServiceImplement.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/servicesimplements/DetalleHistorialServiceImplement.java
@@ -3,15 +3,20 @@ package pe.edu.upc.verisoft_backend_1.servicesimplements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.edu.upc.verisoft_backend_1.entities.DetalleHistorial;
+import pe.edu.upc.verisoft_backend_1.entities.ExamenDeteccion;
 import pe.edu.upc.verisoft_backend_1.repositories.IDetalleHistorialRepository;
 import pe.edu.upc.verisoft_backend_1.servicesinterfaces.IDetalleHistorialService;
+
+import java.util.List;
+
 @Service
 public class DetalleHistorialServiceImplement implements IDetalleHistorialService {
     @Autowired
     private IDetalleHistorialRepository dR;
     @Override
-    public void insert(DetalleHistorial detalleHistorial) {
+    public void insert(DetalleHistorial detalleHistorial) { dR.save(detalleHistorial);}
 
-              dR.save(detalleHistorial);
-    }
+    @Override
+    public List<DetalleHistorial> list() { return dR.findAll();
+        }
 }
