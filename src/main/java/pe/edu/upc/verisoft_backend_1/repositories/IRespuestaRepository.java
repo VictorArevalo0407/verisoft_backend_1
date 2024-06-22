239e32e1dbfcbc26834001f1c63f71859be24a03
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-06-22 16:19:53 -0500

    cambio querys

--- src/main/java/pe/edu/upc/verisoft_backend_1/repositories/IRespuestaRepository.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/repositories/IRespuestaRepository.java
@@ -1,27 +1,25 @@
 package pe.edu.upc.verisoft_backend_1.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.verisoft_backend_1.entities.Respuesta;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public interface IRespuestaRepository extends JpaRepository<Respuesta,Integer> {
 
         //Query 1
-        //Cantidad total de respuestas por grado de consulta para cada paciente
-        @Query(value = "SELECT paciente_id, grado_consulta, COUNT(*) AS cantidad_total_respuestas\n" +
-                "FROM respuesta\n" +
-                "GROUP BY paciente_id, grado_consulta" , nativeQuery = true)
-        public List<String[]> quantityRespuestasByPacienteAndGrado();
+        //Cantidad total de respuestas por estado de paciente
+        @Query("from Respuesta r where r.paciente.estado=:estado")
+        public List<Respuesta>listRespuestaEstado(@Param("estado") String estado);
 
         //Query 2
-        //Cantidad total de respuestas por grado de consulta y estado para cada paciente
-        @Query(value = "SELECT p.id_paciente, p.estado, MAX(r.grado_consulta) as grado_consulta_max, sub.cantidad_total_respuestas\n" +
-                "FROM paciente p JOIN (SELECT paciente_id, COUNT(*) AS cantidad_total_respuestas FROM Respuesta\n" +
-                "GROUP BY paciente_id) sub ON p.id_paciente = sub.paciente_id\n" +
-                "LEFT JOIN Respuesta r ON p.id_paciente = r.paciente_id\n" +
-                "GROUP BY p.id_paciente, p.estado, sub.cantidad_total_respuestas" , nativeQuery = true)
-        public List<String[]> quantityRespuestasByPacienteEstadoAndGrado();
-    }
+        //Cantidad total de respuestas por estado de paciente
+        @Query("from Respuesta r where r.gradoConsulta=:grado")
+        public List<Respuesta>listaRespuestaGrado(@Param("grado") String grado);
+
+
+}
