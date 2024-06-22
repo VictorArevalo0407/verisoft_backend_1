9b6df5d2d576ac2f98151ec6d1358f4511adda86
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-06-22 16:20:05 -0500

    cambio querys

--- src/main/java/pe/edu/upc/verisoft_backend_1/servicesinterfaces/IDetalleHistorialService.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/servicesinterfaces/IDetalleHistorialService.java
@@ -1,7 +1,12 @@
 package pe.edu.upc.verisoft_backend_1.servicesinterfaces;
 
 import pe.edu.upc.verisoft_backend_1.entities.DetalleHistorial;
+import pe.edu.upc.verisoft_backend_1.entities.ExamenDeteccion;
+
+import java.util.List;
 
 public interface IDetalleHistorialService {
     public void insert(DetalleHistorial detalleHistorial);
+    public List<DetalleHistorial> list();
+
 }
