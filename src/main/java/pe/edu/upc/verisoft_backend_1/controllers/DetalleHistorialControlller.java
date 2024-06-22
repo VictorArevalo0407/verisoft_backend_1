94c98905f5ea75abd22c7ce5805e00960ec176e5
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-06-22 16:19:39 -0500

    cambio querys

--- src/main/java/pe/edu/upc/verisoft_backend_1/controllers/DetalleHistorialControlller.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/controllers/DetalleHistorialControlller.java
@@ -2,18 +2,28 @@ package pe.edu.upc.verisoft_backend_1.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.verisoft_backend_1.dtos.DetalleHistorialDTO;
+import pe.edu.upc.verisoft_backend_1.dtos.ExamenDeteccionDTO;
 import pe.edu.upc.verisoft_backend_1.entities.DetalleHistorial;
 import pe.edu.upc.verisoft_backend_1.servicesinterfaces.IDetalleHistorialService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/DetalleHistorial")
 public class DetalleHistorialControlller {
     @Autowired
     private IDetalleHistorialService dS;
+
+    @GetMapping
+    public List<DetalleHistorialDTO> listar() {
+        return dS.list().stream().map(y-> {
+            ModelMapper m=new ModelMapper();
+            return m.map(y,DetalleHistorialDTO.class);
+        }).collect(Collectors.toList());
+    }
 @PostMapping
     public void insertar(@RequestBody DetalleHistorialDTO detalleHistorialDTO) {
     ModelMapper m = new ModelMapper();
