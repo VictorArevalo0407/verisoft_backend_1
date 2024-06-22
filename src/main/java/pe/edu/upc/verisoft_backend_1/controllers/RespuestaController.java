86b24a40129a7cf9b08bddb5e89b0fba09dc63f8
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-06-22 16:19:45 -0500

    cambio querys

--- src/main/java/pe/edu/upc/verisoft_backend_1/controllers/RespuestaController.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/controllers/RespuestaController.java
@@ -3,12 +3,11 @@ package pe.edu.upc.verisoft_backend_1.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.verisoft_backend_1.dtos.PacienteEstadoGradoRespuestaDTO;
-import pe.edu.upc.verisoft_backend_1.dtos.PacienteGradoRespuestaDTO;
 import pe.edu.upc.verisoft_backend_1.dtos.RespuestaDTO;
 import pe.edu.upc.verisoft_backend_1.entities.Respuesta;
 import pe.edu.upc.verisoft_backend_1.servicesinterfaces.IRespuestaService;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,36 +40,25 @@ import java.util.stream.Collectors;
                 }).collect(Collectors.toList());
             }
 
-            @GetMapping("/respuestasPG")
-            public List<PacienteGradoRespuestaDTO> RespuestasPorPacienteYGrado() {
-                List<String[]> filaLista = rS.quantityRespuestasByPacienteAndGrado();
-                List<PacienteGradoRespuestaDTO> dtoLista = new ArrayList<>();
-                for (String[] columna : filaLista) {
-                    PacienteGradoRespuestaDTO dto = new PacienteGradoRespuestaDTO();
-                    dto.setIdPaciente(Integer.parseInt(columna[0]));
-                    dto.setGradoConsulta(columna[1]);
-                    dto.setCantidadTotalRespuestas(Integer.parseInt(columna[2]));
-                    dtoLista.add(dto);
-                }
-                return dtoLista;
-
+            @GetMapping("/consulta01")
+            public List<RespuestaDTO> listaRespuestasEstado(@RequestParam String estado) {
+                return rS.listaRespuestasEstado(estado).stream().map(y -> {
+                    ModelMapper m = new ModelMapper();
+                    return m.map(y, RespuestaDTO.class);
+                }).collect(Collectors.toList());
             }
 
-            @GetMapping("/respuestasPEG")
-            public List<PacienteEstadoGradoRespuestaDTO> RespuestasPorPacienteEstadoYGrado() {
-                List<String[]> filaLista = rS.quantityRespuestasByPacienteEstadoAndGrado();
-                List<PacienteEstadoGradoRespuestaDTO> dtoLista = new ArrayList<>();
-                for (String[] columna : filaLista) {
-                    PacienteEstadoGradoRespuestaDTO dto = new PacienteEstadoGradoRespuestaDTO();
-                    dto.setIdPaciente(Integer.parseInt(columna[0]));
-                    dto.setGradoConsulta(columna[1]);
-                    dto.setEstadoConsulta(columna[2]);
-                    dto.setCantidadTotalRespuestas(Integer.parseInt(columna[3]));
-                    dtoLista.add(dto);
-                }
-                return dtoLista;
+            @GetMapping("/consulta02")
+            public List<RespuestaDTO> listaRespuestaGrado(@RequestParam String grado) {
+                return rS.listaRespuestaGrado(grado).stream().map(y -> {
+                    ModelMapper m = new ModelMapper();
+                    return m.map(y, RespuestaDTO.class);
+                }).collect(Collectors.toList());
             }
         }
+
+
+
 
 
 
