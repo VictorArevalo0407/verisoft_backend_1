edddd1a8451294e1dd9372bb881f2d90ec16d985
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-06-22 16:20:08 -0500

    cambio querys

--- src/main/java/pe/edu/upc/verisoft_backend_1/servicesinterfaces/IRespuestaService.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/servicesinterfaces/IRespuestaService.java
@@ -2,12 +2,13 @@ package pe.edu.upc.verisoft_backend_1.servicesinterfaces;
 import org.springframework.data.repository.query.Param;
 import pe.edu.upc.verisoft_backend_1.entities.Respuesta;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface IRespuestaService {
     public void insert(Respuesta respuesta);
     public List<Respuesta> list();
-    public List<String[]> quantityRespuestasByPacienteAndGrado();
-    public List<String[]> quantityRespuestasByPacienteEstadoAndGrado();
+    public List<Respuesta>listaRespuestasEstado(String estado);
+    public List<Respuesta>listaRespuestaGrado(String grado);
 }
 
